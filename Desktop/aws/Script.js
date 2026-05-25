14d55980834c7a65cc0ba59280bb3c122b36d869
DarlingtonCloud <nkemnebo@gmail.com>
2026-05-25 20:16:20 +0100

    Delete Desktop/aws directory

--- Desktop/aws/Script.js
+++
@@ -1,9 +0,0 @@
-function navigate(section) {
-  // Hide all sections
-  document.querySelectorAll("main section").forEach(sec => {
-    sec.style.display = "none";
-  });
-
-  // Show selected section
-  document.getElementById(section).style.display = "block";
-}
